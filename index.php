ecb08e911278598ca3c04716b5f17771a52abab2
Felix Manugara <marcofel.manugara@gmail.com>
2021-01-20 21:34:25 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width">
+        <meta http-equiv="X-UA-compatible" content="ie-edge">
+        <title>mencoba Git</title>
+    </head>
+    <body>
+        <h3>Data Mahasiswa</h3>
+        <ul>
+            <li>felix marco</li>
+            <li>marco manugara</li>
+            <li>oneokrock</li>
+        </ul>
+    </body>
+</html>
